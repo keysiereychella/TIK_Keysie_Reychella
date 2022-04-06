4f8c867d26aa402e3e286ed963742c5826c23809
keysiereychella <103085827+keysiereychella@users.noreply.github.com>
2022-04-05 20:09:16 -0700

    Create 20220406_Variabel.cpp

---
+++ 20220406_Variabel.cpp
@@ -0,0 +1,14 @@
+//header
+#include <iostream> 
+using namespace std;
+int main() {//begin of function
+    float a;
+    float b;
+    string nama; //deklarasi variabel
+    nama = "Keysie Reychella Gwelisye Kamlasi Tanoen"; //masukkan value ke dalam variabel
+    a = 10.21;
+    b = 3.14
+    cout<<nama<<endl; //menampilkan variabel
+    cout<<a+b;
+    
+}//end of function

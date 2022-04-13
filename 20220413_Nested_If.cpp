e86f7b90791741ecb76054666e8ad5d16b23c9fb
keysiereychella <103085827+keysiereychella@users.noreply.github.com>
2022-04-12 20:29:32 -0700

    Create 20220413_Nested_If.cpp

---
+++ 20220413_Nested_If.cpp
@@ -0,0 +1,33 @@
+//nested if 
+//percabangan bersarang
+//if di dalam if
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int nilai;
+    nilai = 23;
+
+    if (nilai % 2 == 0) {
+        cout<<"Bilangan Genap"<<endl;
+        if (nilai <10) {
+            cout<<"Bilangan genap kurang dari 10"<<endl;
+        }
+        else{
+            cout<<"Bilangan genap lebih dari 10"<<endl;
+        }
+    }
+    else {
+        cout<<"Bilangan Ganjil"<<endl;
+        if (nilai <10) {
+            cout<<"Bilangan ganjil kurang dari 10"<<endl;
+        }
+        else {
+            cout<<"Bilangan ganjil lebih dari 10"<<endl;
+        }
+    }
+    
+   
+    return 0;
+}

a1c6aa1ccf314c9fdf31e7a98a8edc64b307ad35
keysiereychella <103085827+keysiereychella@users.noreply.github.com>
2022-05-10 19:10:59 -0700

    Create 20220511_Array_04.cpp

---
+++ 20220511_Array_04.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int angka[] = {
+        123,
+        123,
+        445,
+        5456,
+        67,
+        6678,
+        867,
+        678,
+        6786,
+        678
+    };
+    for (int i = 0; i<10; i ++){
+        cout<<angka[i]<<endl;
+    }
+    return 0;
+}

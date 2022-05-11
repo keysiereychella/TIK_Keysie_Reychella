99da97ab49b34708940fac7eb7d871373ae1c108
keysiereychella <103085827+keysiereychella@users.noreply.github.com>
2022-05-10 18:53:05 -0700

    Create 20220511_Array_01.cpp

---
+++ 20220511_Array_01.cpp
@@ -0,0 +1,16 @@
+//array adalah variabel penyimpanan untuk tipe data sama
+//urutan data di array dimulai dari 0 
+//ditandai dengan bracket []
+#include <iostream>
+using namespace std;
+
+int main (){
+    string nama[] = {
+        "David",
+        "Kabila",
+        "Bryan",
+        "Sidney"
+    };
+    cout<<nama[2];
+    return 0;
+}
